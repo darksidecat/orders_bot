4431ac1e8900420248f1d2d22bd13be58198cbec
darksidecat <bitalik371@gmail.com>
2022-06-13 00:13:22 +0300

    WIP

--- app/infrastructure/database/models/goods.py
+++ app/infrastructure/database/models/goods.py
@@ -1,4 +1,4 @@
-from sqlalchemy import BOOLEAN, TEXT, Column
+from sqlalchemy import BOOLEAN, TEXT, Column, Computed, ForeignKeyConstraint
 from sqlalchemy import Enum as SQLEnum
 from sqlalchemy import ForeignKey, Table, UniqueConstraint, func
 from sqlalchemy.dialects.postgresql import UUID
@@ -8,6 +8,47 @@ from app.domain.goods.models.goods import Goods
 from app.domain.goods.models.goods_type import GoodsType
 
 from .base import mapper_registry
+
+
+nomenclature_table = Table(
+    "nomenclature",
+    mapper_registry.metadata,
+    Column("id", UUID(as_uuid=True), primary_key=True, default=func.uuid_generate_v4()),
+    Column("type", SQLEnum(GoodsType), nullable=False),
+    Column("name", TEXT, nullable=False),
+    UniqueConstraint("id", "type", name="uq_nomenclature_id_type"),
+)
+
+folder_table = Table(
+    "folder",
+    mapper_registry.metadata,
+    Column("id", UUID(as_uuid=True), primary_key=True),
+    Column("type", SQLEnum(GoodsType), Computed("FOLDER"), nullable=False),
+    Column("parent_id", UUID(as_uuid=True), ForeignKey("folder.id")),
+    ForeignKeyConstraint(
+        ["id", "type"],
+        ["nomenclature.id", "nomenclature.type"],
+        name="fk_folder_nomenclature",
+        ondelete="CASCADE",
+        onupdate="CASCADE",
+    ),
+)
+
+goods_table = Table(
+    "goods",
+    mapper_registry.metadata,
+    Column("id", UUID(as_uuid=True), primary_key=True),
+    Column("type", SQLEnum(GoodsType), Computed("GOODS"), nullable=False),
+    Column("parent_id", UUID(as_uuid=True), ForeignKey("folder.id")),
+    Column("sku", TEXT, nullable=False),
+    ForeignKeyConstraint(
+        ["id", "type"],
+        ["nomenclature.id", "nomenclature.type"],
+        name="fk_folder_nomenclature",
+        ondelete="CASCADE",
+        onupdate="CASCADE",
+    ),
+)
 
 goods_table = Table(
     "goods",
