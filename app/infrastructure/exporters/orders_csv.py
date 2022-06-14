a8fd730e59c8acc2d52f77cd3dfab7db578fed29
darksidecat <bitalik371@gmail.com>
2022-06-15 00:20:30 +0300

    add orders export to csv

---
+++ app/infrastructure/exporters/orders_csv.py
@@ -0,0 +1,47 @@
+import csv
+import io
+
+from app.domain.order.dto.order import Order
+
+
+# function that get list of orders and save them to csv file in memory as bytes
+def export_orders_to_csv(orders: list[Order]) -> bytes:
+    # create csv writer
+    output = io.StringIO()
+    csv_writer = csv.writer(output)
+    # write header
+    csv_writer.writerow(
+        [
+            "id",
+            "creator_id",
+            "creator_name",
+            "recipient_market_id",
+            "recipient_market_name",
+            "goods_name",
+            "goods_sku",
+            "quantity",
+            "commentary",
+            "created_at",
+            "confirmed",
+        ]
+    )
+    # write orders
+    for order in orders:
+        for line in order.order_lines:
+            csv_writer.writerow(
+                [
+                    order.id,
+                    order.creator.id,
+                    order.creator.name,
+                    order.recipient_market.id,
+                    order.recipient_market.name,
+                    line.goods.name,
+                    line.goods.sku,
+                    line.quantity,
+                    order.commentary,
+                    order.created_at,
+                    order.confirmed,
+                ]
+            )
+    # return bytes
+    return output.getvalue().encode("utf-8")
--- app/tgbot/handlers/user/history.py
+++ app/tgbot/handlers/user/history.py
@@ -1,6 +1,7 @@
 import math
+from typing import Optional
 
-from aiogram.types import CallbackQuery
+from aiogram.types import BufferedInputFile, CallbackQuery, InputFile
 from aiogram.utils.text_decorations import html_decoration as fmt
 from aiogram_dialog import Dialog, DialogManager, Window
 from aiogram_dialog.widgets.kbd import Back, Button, Cancel, Next, Row
@@ -9,6 +10,7 @@ from aiogram_dialog.widgets.text import Const, Format
 from app.domain.access_levels.models.access_level import LevelName
 from app.domain.order.usecases.order import OrderService
 from app.domain.user.dto import User
+from app.infrastructure.exporters.orders_csv import export_orders_to_csv
 from app.tgbot.handlers.user.common import format_order_message
 from app.tgbot.states import history
 
@@ -41,28 +43,63 @@ async def history_access_getter(dialog_manager: DialogManager, user: User, **kwa
     return access_levels
 
 
+async def import_orders_csv(
+    query: CallbackQuery, button: Button, manager: DialogManager, **kwargs
+):
+    history_level = manager.current_context().dialog_data["history_level"]
+    order_service = manager.data["order_service"]
+    user = manager.data["user"]
+
+    orders, _ = await get_orders(
+        order_service=order_service,
+        user=user,
+        history_level=history_level,
+        offset=0,
+        limit=None,
+    )
+
+    csv = export_orders_to_csv(orders)
+    await query.message.answer_document(BufferedInputFile(csv, filename="orders.csv"))
+
+
+async def get_orders(
+    order_service: OrderService,
+    user: User,
+    history_level: str,
+    offset: int,
+    limit: Optional[int] = ORDERS_ON_PAGE_LIMIT,
+):
+    if history_level == MY_ORDERS:
+        orders = await order_service.get_user_orders(
+            user_id=user.id, offset=offset, limit=limit
+        )
+        orders_count = await order_service.get_user_orders_count(user_id=user.id)
+    elif history_level == ORDERS_FOR_CONFIRMATION:
+        orders = await order_service.get_orders_for_confirmation(
+            offset=offset, limit=limit
+        )
+        orders_count = await order_service.get_orders_for_confirmation_count()
+    elif history_level == ALL_ORDERS:
+        orders = await order_service.get_all_orders(offset=offset, limit=limit)
+        orders_count = await order_service.get_all_orders_count()
+    else:
+        raise ValueError(f"Unknown history level: {history_level}")
+
+    return orders, orders_count
+
+
 async def orders_getter(
     dialog_manager: DialogManager, order_service: OrderService, user: User, **kwargs
 ):
     history_level = dialog_manager.current_context().dialog_data["history_level"]
     offset = dialog_manager.current_context().dialog_data.setdefault("offset", 0)
-    if history_level == MY_ORDERS:
-        orders = await order_service.get_user_orders(
-            user_id=user.id, offset=offset, limit=ORDERS_ON_PAGE_LIMIT
-        )
-        orders_count = await order_service.get_user_orders_count(user_id=user.id)
-    elif history_level == ORDERS_FOR_CONFIRMATION:
-        orders = await order_service.get_orders_for_confirmation(
-            offset=offset, limit=ORDERS_ON_PAGE_LIMIT
-        )
-        orders_count = await order_service.get_orders_for_confirmation_count()
-    elif history_level == ALL_ORDERS:
-        orders = await order_service.get_all_orders(
-            offset=offset, limit=ORDERS_ON_PAGE_LIMIT
-        )
-        orders_count = await order_service.get_all_orders_count()
-    else:
-        raise ValueError(f"Unknown history level: {history_level}")
+
+    orders, orders_count = await get_orders(
+        order_service=order_service,
+        user=user,
+        history_level=history_level,
+        offset=offset,
+    )
 
     message = f"Orders:\n\nPage: {offset // ORDERS_ON_PAGE_LIMIT + 1}/{math.ceil(orders_count / ORDERS_ON_PAGE_LIMIT)}\n\n"
     for order in orders:
@@ -146,7 +183,9 @@ history_dialog = Dialog(
                 when="has_next",
             ),
         ),
-        Button(Const("💾 Import data"), id="import_data"),
+        Button(
+            Const("💾 Import data"), id="import_orders_csv", on_click=import_orders_csv
+        ),
         Back(Const("↩️ Back to history"), on_click=reset_offset),
         Cancel(Const("❌ Close")),
         state=history.History.show,
